caa8a8fd787d33037c1cfa600aedd5b1cb5e78a3
Julexy Bajaña <julexybajana@gmail.com>
2023-09-14 00:03:21 -0500

    ready for render

--- django_portafolio/settings.py
+++ django_portafolio/settings.py
@@ -49,13 +49,13 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
 
 ROOT_URLCONF = 'django_portafolio.urls'
@@ -84,7 +84,7 @@ WSGI_APPLICATION = 'django_portafolio.wsgi.application'
 
 DATABASES = {
     'default':dj_database_url.config(
-        default= 'postgresql://postgres:postgres@localhost/postgres',
+        default= 'sqlite:///db.sqlite3',
         conn_max_age=600
     ) }
 
